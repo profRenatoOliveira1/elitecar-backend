03984c9f76f833462b5e79ca42d42fd42a600001
Renato Oliveira <renato.loliveira89@gmail.com>
2024-10-01 11:41:26 -0300

    Criando servidor web

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Request, Response, Router } from "express";
+
+// Cria um roteador
+const router = Router();
+
+// Criando uma rota principal para a aplicação
+router.get("/", (req: Request, res: Response) => {
+    res.json({ mensagem: "Olá, mundo!" });
+});
+
+// exportando as rotas
+export { router };
