909571f12dd03338e9336f6be497573ddfe3948d
Renato Oliveira <renato.loliveira89@gmail.com>
2024-10-17 15:59:46 -0300

    Ajustando valor_pedido para um DECIMAL(8,2)

--- infra/sql/init.sql
+++ infra/sql/init.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS pedido_venda (
     id_cliente INT NOT NULL,
     id_carro INT NOT NULL,
     data_pedido DATE NOT NULL,
-    valor_pedido DECIMAL(6) NOT NULL,
+    valor_pedido DECIMAL(8,2) NOT NULL,
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
     FOREIGN KEY (id_carro) REFERENCES carro(id_carro)
 );
