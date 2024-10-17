4e3e6bd79a9b76b01ec7258683bf61349c7bfb99
Renato Oliveira <renato.loliveira89@gmail.com>
2024-10-17 13:27:53 -0300

    Implementando e testando conexão com o banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import { server } from './server';
+import { DatabaseModel } from './model/DatabaseModel';
 
 const port: number = 3333;
 
-server.listen(port, () => {
-  console.log(`Endereço do servidor: http://localhost:${port}`);
+// só vou ligar SE o banco estiver funcionando
+new DatabaseModel().testeConexao().then((resdb) => {
+  if (resdb == true) {
+    server.listen(port, () => {
+      console.log(`Endereço do servidor: http://localhost:${port}`);
+    });
+  } else {
+    console.log(`Erro ao conectar com o banco de dados.`);
+  }
 });
